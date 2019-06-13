9396ad1c30b04b481fdfa542a802d60036606b8a
Rina Razumov <rinash3@gmail.com>
2019-06-12 22:46:55 -0400

    crreated files

---
+++ assets/javascript/trainTime.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCOy8dpg91UlNVXF4LhaQN7eLqj2FdLYpo",
+    authDomain: "traintime-5d9fd.firebaseapp.com",
+    databaseURL: "https://traintime-5d9fd.firebaseio.com",
+    projectId: "traintime-5d9fd",
+    storageBucket: "",
+    messagingSenderId: "1022334996187",
+    appId: "1:1022334996187:web:190ee146a2fce94c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
